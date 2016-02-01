f86c7ac5ad4f842be0fc33e441640cccd3336b75
Santiago Kelley <sakp97@me.com>
2016-02-01 12:08:58 -0600

    Added Proxy files

--- Coldcraft/src/java/com/fawtytwo/coldcraft/Main.java
+++ Coldcraft/src/java/com/fawtytwo/coldcraft/Main.java
@@ -3,6 +3,7 @@ package com.fawtytwo.coldcraft;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -16,6 +17,9 @@ public class Main {
 
     @Instance
     public static Main instance = new Main();
+    
+    @SidedProxy(clientSide="com.fawtytwo.coldcraft.ClientProxy", serverSide="com.fawtytwo.coldcraft.ServerProxy")
+    public static CommonProxy proxy;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent e) {
